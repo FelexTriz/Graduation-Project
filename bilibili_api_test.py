b75182f73b07aa0bc11583b37155ce7241e44fe9
FelexTriz <1229915477@qq.com>
2023-03-06 22:49:44 +0800

    测试

--- bilibili_api_test.py
+++ bilibili_api_test.py
@@ -1,9 +1,21 @@
 import requests
 import json
+
+# 获取热门视频
 remen = requests.get('https://api.bilibili.com/x/web-interface/popular')
 remen_data = json.loads(remen.text)
-remen2= remen_data['data']['list'][1]['tname']
-print(remen2)
+
+# 获得单个视频信息
+for video in remen_data['data']['list']:
+    video_title = video['title']
+    video_bvid = video['bvid']
+    video_tid = video['tid']
+    video_owner = video['owner']
+
+# remen2= remen_data['data']['list'][0]['title']
+# print(remen2)
+
+
 # file = open("./test1.txt",mode='w')
 # file.write(remen2)
 # file.close
